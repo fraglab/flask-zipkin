21bc31b64088b05a3d4e4618eea193e8fd78e539
defat <defat@ukr.net>
2017-06-20 17:35:02 +0300

    version increment

--- flask_zipkin.py
+++ flask_zipkin.py
@@ -9,7 +9,7 @@ import requests
 from py_zipkin import zipkin
 
 
-__version_info__ = ('0', '0', '2')
+__version_info__ = ('0', '0', '3')
 __version__ = '.'.join(__version_info__)
 __author__ = 'killpanda'
 __license__ = 'BSD'
